94bf95c312395a1693b81fc5a0706c6ee2b8b550
Bogdan <bbogdan@ymail.com>
2022-09-12 16:24:40 +0300

    some testing and learning how the flow works

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+names = {"tim": {"age": 19, "gender": "male"}, "bill": {"age": 70, "gender": "male"}}
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
+
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+    def get(self, name, test):
+        print(request.form)
+        return names[name]
 
     def post(self):
         return {"data": "Posted"}
 
+    def put(self, name, test):
+        args = video_put_args.parse_args()
+        return {"args": args}
 
-api.add_resource(HelloWorld, "/helloworld")
+
+videos = {}
+
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form)
+        return {}
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "helloworld")
+# response = requests.get(BASE + "helloworld")
+# response = requests.get(BASE + "helloworld")
+response = requests.put(BASE + "helloworld/bill/19", {"likes": 10})
+# response = requests.put(BASE + "video")
+
 print(response.json())
